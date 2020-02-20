b1cfa84e77fa71e78f89103590df5877aefc6516
Mario Daniel Gallardo Cruzado <mariodga@ucm.es>
2020-02-20 19:57:24 +0100

    codigo sin probar para puntos 2 dimensiones

--- es/ucm/fdi/pe/Main.java
+++ es/ucm/fdi/pe/Main.java
@@ -9,24 +9,28 @@ public class Main{
 		double probCruce = 0.6;
 		double probMutacion = 0.01;
 		double precision=0.001;
-		double xmax=0;
-		double xmin=10;
+		double xmax=10;
+		double xmin=0;
 		double x = 1+(xmax-xmin)/precision;
 		int nbits = (int)(Math.log(x)/Math.log(2));
 
-		Punto[] ptos = new Punto[]{
+		Punto[] pob = new Punto[]{
 			new Punto(){{x=1;y=2;}},
-			new Punto(){{x=1;y=2;}}
+			new Punto(){{x=2;y=2;}},
+			new Punto(){{x=3;y=2;}},
+			new Punto(){{x=1;y=3;}}
 		};
 
-		Integer[] pob = new Integer[]{1,2};
-		ArrayList<Integer> npob = new ArrayList<>(){};
-		Fitness<Integer> tf = i->{
-			double g = xmin + i*(xmax-xmin)/(Math.pow(2,nbits)-1);
-			return (int)(1 - (g*g));
+		ArrayList<Punto> npob = new ArrayList<>(){};
+		Fitness<Punto> tf = i->{
+			double pi = Math.PI;
+			double cx = xmin + i.x*(xmax-xmin)/(Math.pow(2,nbits)-1);
+			double cy = xmin + i.y*(xmax-xmin)/(Math.pow(2,nbits)-1);
+			double res = 21.5 + cx*Math.sin(4*pi*cx)+cy*Math.sin(20*pi*cy);
+			return (int)(-res);
 		};
-		Seleccion<Integer> ts = p->{//Seleccion por ruleta
-			Integer[] s = new Integer[2];
+		Seleccion<Punto> ts = p->{//Seleccion por ruleta
+			Punto[] s = new Punto[2];
 			double r1 = Math.random();
 			double r2 = Math.random();
 			int pacc = 0;
@@ -39,7 +43,7 @@ public class Main{
 			}
 			return p;
 		};
-		Cruce<Integer> tc = (p1, p2)->{//monopunto
+		Cruce<Punto> tc = (p1, p2)->{//monopunto
 			double prob = 1/nbits;
 			double pacc = 0;
 			double r = Math.random();
@@ -49,16 +53,34 @@ public class Main{
 				pacc+=prob;
 				i++;
 			}
-			String h1rep = Integer.toBinaryString(p1);
-			String h2rep = Integer.toBinaryString(p2);
+			String h1rep = Integer.toBinaryString(p1.x);
+			String h2rep = Integer.toBinaryString(p2.x);
 			String h1 = h1rep.substring(0,i).concat(h2rep.substring(i,nbits));
 			String h2 = h2rep.substring(0,i).concat(h1rep.substring(i,nbits));
 			Integer a = Integer.parseInt(h1,2);
 			Integer b = Integer.parseInt(h2,2);
-			return new Integer[]{a,b};
+
+			r = Math.random();
+			i = 0;
+			while(i<nbits){
+				if(r<=pacc) break;
+				pacc+=prob;
+				i++;
+			}
+			h1rep = Integer.toBinaryString(p1.y);
+			h2rep = Integer.toBinaryString(p2.y);
+			h1 = h1rep.substring(0,i).concat(h2rep.substring(i,h2.length()));
+			h2 = h2rep.substring(0,i).concat(h1rep.substring(i,h1.length()));
+			Integer c = Integer.parseInt(h1,2);
+			Integer d = Integer.parseInt(h2,2);
+
+			return new Punto[]{
+				new Punto(){{x=a;y=c;}},
+				new Punto(){{x=b;y=d;}},
+			};
 		};
-		Mutacion<Integer> tm = h->{//mutacion basica
-			String brep = Integer.toBinaryString(h);
+		Mutacion<Punto> tm = h->{//mutacion basica
+			String brep = Integer.toBinaryString(h.x);
 			StringBuffer sb = new StringBuffer();
 			for(char c:brep.toCharArray()){
 				double r = Math.random();
@@ -67,11 +89,24 @@ public class Main{
 					else sb.append('1');
 				}
 			}
-			return Integer.parseInt(sb.toString());
+			Integer nx = Integer.parseInt(sb.toString());
+
+			brep = Integer.toBinaryString(h.y);
+			sb = new StringBuffer();
+			for(char c:brep.toCharArray()){
+				double r = Math.random();
+				if(r<probMutacion) {
+					if(c=='0') sb.append('0');
+					else sb.append('1');
+				}
+			}
+			Integer ny = Integer.parseInt(sb.toString());
+			return new Punto(){{x=nx;y=ny;}};
 		};
+
 		while(generacion<nIteraciones){
 			for(int i=0; i<(pob.length/2); i++){
-				Integer[] sel, hijos, mutados;
+				Punto[] sel, hijos, mutados;
 				sel = seleccion(ts, pob);
 				hijos = cruce(tc, sel, probMutacion);
 				mutados = mutacion(tm, hijos, probMutacion);
@@ -80,6 +115,8 @@ public class Main{
 			pob=npob.toArray(pob);
 			generacion++;
 		}
+		System.out.println(Arrays.toString(pob));
+
 	}
 	public static <T> T[] seleccion(Seleccion<T> tsel, T[] poblacion){
 		return tsel.execute(poblacion);
@@ -114,7 +151,12 @@ interface Fitness<G>{
 	int fitness(G genotipe);
 }
 
-class Punto{public Integer x,y;}
+class Punto{
+	public Integer x,y;
+	public String toString(){
+		return "x:"+x+"y:"+y;
+	}
+}
 //System.out.println(Arrays.toString(s));
 //tamPoblacion
 //seleccion() 	ruleta, estocastico, torneo
